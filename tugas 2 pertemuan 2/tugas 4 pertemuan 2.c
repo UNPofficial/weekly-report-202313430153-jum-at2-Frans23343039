36de252d2e8c4e05d76bb2c28a90e1d039f9d93c
Frans23343039 <145112143+Frans23343039@users.noreply.github.com>
2023-09-15 16:29:04 +0700

    Add files via upload

---
+++ tugas 2 pertemuan 2/tugas 4 pertemuan 2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Created by 23343039-Frans
+
+int main() {
+	//_---------luas persegi panjang---------_//
+	
+	//deklarasi 
+	double panjang = 11.0;
+	double lebar = 6.0;
+	double luas;
+
+	// Menghitung luas persegi panjang 
+	luas = panjang * lebar;
+	
+	// Menampilkan hasil menghitung luas persegi panjang 
+	printf("Panjang: %.2lf cm\n", panjang);
+    printf("Lebar: %.2lf cm\n", lebar);
+    printf("Maka Luas Persegi Panjang Adalah %.2lf cm^2\n", luas);
+
+    return 0;
+}
+
+{
+}
