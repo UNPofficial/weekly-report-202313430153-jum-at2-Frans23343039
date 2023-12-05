0fb8f8afe1174f13665b617f0071721b7c0abfbd
Frans23343039 <145112143+Frans23343039@users.noreply.github.com>
2023-12-05 18:02:10 +0700

    Add files via upload

---
+++ Jobsheet 3/Variabel,konstanta,dan tipe data/Latihan/Program/Latihan 4_Jobsheet 3_Konstata menggunakan const.c
@@ -0,0 +1,15 @@
+//Created by Frans surya pati harau_23343039
+
+//Latihan 4_Jobsheet 3_Konstanta menggunakan const
+
+#include <stdio.h>
+
+void main(){
+    const double PI = 3.14;
+    const char JENIS_KELAMIN = 'P';
+    const int VERSI = 11;
+
+    printf("isi konstanta PI adalah %f\n", PI);
+    printf("isi konstanta JENIS_KELAMIN adalah %c\n", JENIS_KELAMIN);
+    printf("isi konstanta VERSI adalah %i\n", VERSI);
+}
